39542654440312ac0c5bc7c2f3e3e193be25062f
Mohamed_Yahyaoui <61945346+Mohamed87Yahyaoui@users.noreply.github.com>
2022-06-09 00:12:05 +0100

    caf rbfr sbfr

--- main/autofinancement1.py
+++ main/autofinancement1.py
@@ -4,21 +4,21 @@ import pandas as pd
 with st.form(key="autofinancement1"):
    titre = st.text_input("Titre:")
    inv = st.text_input("Investissement:")
-   ca = float(st.text_input("Chiffre d\'affaire:"))
-   vac = float(st.text_input("Vaiation du chiffre d'affaire en %"))
-   ta = float(st.text_input("Taux d'amortissement en %"))
+   ca = st.text_input("Chiffre d'affaire:")
+   vac = st.text_input("Vaiation du chiffre d'affaire en %")
+   ta = st.text_input("Taux d'amortissement en %")
    ra = st.text_input("Régime d'amortissement", value="Linéaire")
-   vr = float(st.text_input("Valeur résiduelle"))
-   cf = float(st.text_input("Charges fixes"))
-   cv = float(st.text_input("Charges variables en %"))
-   bfr = float(st.text_input("Besoin en fond de roulement en %"))
-   rbfr = float(st.text_input("Recupération du besoin en fond de roulement en %"))
+   vr = st.text_input("Valeur résiduelle")
+   cf = st.text_input("Charges fixes")
+   cv = st.text_input("Charges variables en %")
+   bfr = st.text_input("Besoin en fond de roulement en %")
+   rbfr = st.text_input("Recupération du besoin en fond de roulement en %")
 
    submissionButton = st.form_submit_button(label="Valider")
    if submissionButton==True:
-      cvauto = (cv / 100) * ca
-      amorti = (ta / 100) * inv
-      rbrute = ca - cvauto - cf - amorti
+      cvauto = (float(cv) / 100) * float(ca)
+      amorti = (float(ta) / 100) * float(inv)
+      rbrute = float(ca) - cvauto - float(cf) - amorti
       rnet = rbrute - (0.3 * rbrute)
       caf = rnet + amorti
 
@@ -28,30 +28,50 @@ with st.form(key="autofinancement1"):
       st.write('rnet ' + str(rnet))
       st.write('caf ' + str(caf))
 
-      nbannee=5
+      nbannee = 5
 
-      caList=[]
-      cafList=[]
-      bfrList=[]
+      caList = []
+      cafList = []
+      bfrList = []
       cafList.append("RAS")
       cafList.append(caf)
-      caList.append(ca)
-      for i in range(nbannee-2):
-         ca = ca + ca * (vac / 100)
+      caList.append(float(ca))
+      for i in range(nbannee - 1):
+         ca = float(ca) + float(ca) * (float(vac) / 100)
          caList.append(ca)
-         cvauto = (cv / 100) * ca
-         rbrute = ca - cvauto - cf - amorti
+         cvauto = (float(cv) / 100) * float(ca)
+         rbrute = float(ca) - cvauto - float(cf) - amorti
          rnet = rbrute - (0.3 * rbrute)
          caf = rnet + amorti
          cafList.append(caf)
       print(cafList)
 
-      for i in range(nbannee-1):
-         bfrList.append((bfr/100)*caList[i])
+      #Reccup bfr
+      Sbfr=0
+      for i in range(nbannee):
+         bfrList.append((float(bfr) / 100) * caList[i])
+         Sbfr=bfrList[i]+Sbfr
+
+      Rbfr=Sbfr*float(rbfr)
+      print(Rbfr)
+
+      #Variation du besoin de fond de roulement
+
+      Vbfr=[]
+      Vbfr.append(bfrList[0])
+      for i in range(1,nbannee):
+         Vbfr.append(bfrList[i]-bfrList[i-1])
+
+      print(Vbfr)
+
+      #van
 
 
-      for i in range(nbannee-2):
-         rbfrList.append('RAS')
+
+
+
+
+
 
 
 
