681a996f40ee9b69001e432b9657e69213917102
Mohamed_Yahyaoui <61945346+Mohamed87Yahyaoui@users.noreply.github.com>
2022-06-09 14:18:51 +0100

    Multipage app

---
+++ main/apps/login.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import sqlite3
+import hashlib
+
+def app():
+   conn=sqlite3.connect('data.db', check_same_thread=False)
+   cur=conn.cursor()
+
+   def make_hashes(password):
+	   return hashlib.sha256(str.encode(password)).hexdigest()
+
+   def login_user(username,password):
+	   conn.execute('SELECT * FROM USER WHERE email =? AND password = ?',(email,make_hashes(password)))
+	   data = cur.fetchall()
+	   return data
+
+
+   with st.form(key="login"):
+      email = st.text_input('Email:')
+      password = st.text_input("Enter a password", type="password")
+
+      submissionButton = st.form_submit_button(label="Sign up")
+      if submissionButton == True:
+         user=login_user(email,make_hashes(password))
+         print(user)
+         if user:
+            print(user)
+            st.success("Logged In as {}".format(user.fname+user.lname))
+
+
+
+
+
